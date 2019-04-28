25b2e4437bb9c4dcfa0eeb9233ce5735b9085e55
Bartłomiej Karwowski <karwowski.bart@gmail.com>
2019-04-28 13:39:30 +0200

    Start coding TopDef checks

--- src/CompileCheck.hs
+++ src/CompileCheck.hs
@@ -1,9 +1,28 @@
 module CompileCheck where
 
 import Data.Map
+import Control.Monad.State
 
 import AbsGram
 
 import ErrM
 
+type Var = String
+type Loc = Int
+data Val = VInt Int | VBool Bool | VStr String
+type FVal = ([Arg], Block)
 
+type Env = (Map Var Loc, Map Var Type, Map Loc Val, Map Var FVal)
+
+printBad (Bad s) = s
+
+checkTopDef :: Program -> Err Env
+checkTopDef prog = case runStateT (checkTopDef' prog) (empty, empty, empty, empty) of
+    Ok ((),s) -> Ok s
+    Bad e     -> Bad e 
+
+checkTopDef' :: Program -> StateT Env Err ()
+checkTopDef' (Program topdefs) = mapM_ checkTopDef'' topdefs
+
+checkTopDef'' :: TopDef -> StateT Env Err ()
+checkTopDef'' t = state $ \s -> ((),s)
