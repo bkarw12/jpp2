65af4a44791df1ecf425698bd87eaaed3e39f2ac
Bartłomiej Karwowski <karwowski.bart@gmail.com>
2019-05-09 00:35:43 +0200

    Finish refactoring error messages

--- src/CompileCheck.hs
+++ src/CompileCheck.hs
@@ -50,19 +50,39 @@ eVarDeclType var = "types mismatch in variable declaration: " ++ var
 eVarRedecl var = "variable redeclaration: " ++ var
 eVarRedeclRead var = "cannot redeclare read-only variable: " ++ var
 
+eVarGlobalType var = "types mismatch for global variable assignment: " ++ var
+eVarGlobalRedecl var = "global variable redeclaration: " ++ var
+eVarGlobalAss = "expression assigned to global variable is not a constant value"
+
 eVarVoid = "variable declared with wrong type: void"
 eVarFun = "variable declared with wrong type: fun"
 
+eFunUndeclared var = "function not declared: " ++ var
+eFunRedecl var = "function redeclaration: " ++ var
+eFunPars fun = "wrong number of arguments in function: " ++ printTree fun
+eFunType fun = "types mismatch in function statement: " ++ printTree fun
+
 eTypeAss ass = "types mismatch in variable assignment: " ++ printTree ass
+eTypeRet ret = "types mismatch in return: " ++ printTree ret
 
 eTypeInc id = "incremented variable not an int: " ++ printTree id
 eTypeDec id = "decremented variable not an int: " ++ printTree id
 eTypeFor id = "for loop argument not an int: " ++ printTree id
+eTypeFor1 e = "for loop start expression is not an int: " ++ printTree e
+eTypeFor2 e = "for loop end expression is not an int: " ++ printTree e
 eTypeInt e = "expression not an int: " ++ printTree e
 eTypeNegInt e = "negated expression not an int: " ++ printTree e
+
 eTypeBool e = "expression not a boolean: " ++ printTree e
 eTypeNotBool e = " \"Not\" expression not a boolean: " ++ printTree e
 eTypeCond e = "condition expression not a boolean: " ++ printTree e
+eTypeWhile e = "while loop condition expression is not a boolean: " ++ printTree e
+
+eMainNotFound = "main function not found"
+eMainRet = "main function return type should be int"
+eMainArgs = "main function does not take any arguments"
+
+eNonVoidEnd var = "reached end of non-void function " ++ var
 
 -- Auxillary functions
 
@@ -73,8 +93,6 @@ newLoc :: Map Loc a -> Loc
 newLoc m 
     | Data.Map.null m    = 0
     | otherwise = (fst $ findMax m) + 1
-
--- TODO: generalize these getters?
 
 getLoc :: Var -> Stt Loc
 getLoc var = do
@@ -210,8 +228,8 @@ tcStmt (CondElse e stmt1 stmt2) = do
     tcStmt stmt2
 tcStmt (For id e1 e2 stmt) = do
     tcExpr'NoInit Int (EVar id) $ eTypeFor id
-    tcExpr' Int e1 $ "Error: For loop start expression " ++ show e1 ++ " is not an int."
-    tcExpr' Int e2 $ "Error: For loop end expression " ++ show e1 ++ " is not an int."
+    tcExpr' Int e1 $ eTypeFor1 e1
+    tcExpr' Int e2 $ eTypeFor2 e2
     let (Ident var) = id
     initVar var
     env <- get
@@ -219,18 +237,18 @@ tcStmt (For id e1 e2 stmt) = do
     put env {rEnv = rs'}
     tcStmt stmt
 tcStmt (While e stmt) = do
-    tcExpr' Bool e $ "Error: While loop condition expression " ++ show e ++ " is not a boolean."
+    tcExpr' Bool e $ eTypeWhile e
     tcStmt stmt
 tcStmt (SExp e) = do 
     tcExpr e
     return ()
 tcStmt (Ret e) = do
     env <- get
-    tcExpr' (ret env) e $ "Error: Types mismatch in return: " ++ show (Ret e) ++ "."
+    tcExpr' (ret env) e $ eTypeRet (Ret e)
 tcStmt VRet = do
     env <- get
     if ret env == Void then return ()
-    else lift $ Bad $ "Error: Types mismatch in return: " ++ show VRet ++ "."
+    else liftError $ eTypeRet VRet
 tcStmt _ = return ()
 
 tcExpr :: Expr -> Stt Type
@@ -243,11 +261,11 @@ tcExpr ELitFalse = return Bool
 tcExpr (EApp (Ident var) es) = do
     env <- get
     case Data.Map.lookup var $ fEnv env of
-        Nothing         -> lift $ Bad $ "Error: function \"" ++ show var ++ "\" not declared."
+        Nothing         -> liftError $ eFunUndeclared var
         Just (t,args,_) -> do
             checkArgs args es 
-                ("Error: Wrong number of parameters in function statement: " ++ show (EApp (Ident var) es) ++ ".")
-                ("Error: Types mismatch in function statement: " ++ show (EApp (Ident var) es) ++ ".")
+                (eFunPars (EApp (Ident var) es))
+                (eFunType (EApp (Ident var) es))
             return t
 tcExpr (EString _) = return Str
 tcExpr (Neg e) = do
@@ -304,7 +322,7 @@ tcVars' ls (loc,vval) = case vval of
     (Int, VInt,_)   -> Ok ()
     (Bool, VBool,_) -> Ok ()
     (Str, VStr,_)   -> Ok ()
-    _               -> Bad $ "Error: types mismatch for global variable \"" ++ (keyFromValue ls loc) ++ "\" assignment."
+    _               -> Bad $ eVarGlobalType $ keyFromValue ls loc
 
 tcFuncs :: Env -> Err ()
 tcFuncs env = mapM_ (tcFuncs' env) $ toList $ fEnv env
@@ -330,7 +348,7 @@ checkTopDef'' (FnDef t (Ident var) args b) = do
     if notMember var fs then 
         let fs' = insert var (t,args,b) fs 
         in put env {fEnv = fs'}
-    else lift $ Bad $ "Error: Function redeclaration: " ++ var ++ "."
+    else liftError $ eFunRedecl var
 checkTopDef'' (VDef (Decl t items)) = mapM_ (checkTopDefV t) items
 
 checkTopDefV :: Type -> Item -> Stt ()
@@ -349,27 +367,27 @@ checkTopDefV' val t var = do
             ls' = insert var loc ls
             vs' = insert loc (t,val,0) vs
         in put env {lEnv = ls', vEnv = vs'}
-    else lift $ Bad $ "Error: Global variable redeclaration: " ++ var ++ "."
+    else liftError $ eVarGlobalRedecl var
 
 calcTopDefVal :: Expr -> Stt Val
 calcTopDefVal (ELitInt _) = lift $ Ok VInt
 calcTopDefVal ELitTrue = lift $ Ok VBool
 calcTopDefVal ELitFalse = lift $ Ok VBool
 calcTopDefVal (EString _) = lift $ Ok VStr
-calcTopDefVal (Neg e) = calcTopDefVal' e VInt $ "Error: Negated expression " ++ show e ++ " not an int."
-calcTopDefVal (Not e) = calcTopDefVal' e VBool $ "Error: \"Not\" expression " ++ show e ++ " not a boolean."
+calcTopDefVal (Neg e) = calcTopDefVal' e VInt $ eTypeNegInt e
+calcTopDefVal (Not e) = calcTopDefVal' e VBool $ eTypeNotBool e
 calcTopDefVal (EMul e1 _ e2) = calcTopDefVal2' e1 e2 VInt
-    ("Error: Expression " ++ show e1 ++ " not an int.")
-    ("Error: Expression " ++ show e2 ++ " not an int.")
+    (eTypeInt e1)
+    (eTypeInt e2)
 calcTopDefVal (EAdd e1 _ e2) = calcTopDefVal (EMul e1 Times e2)
 calcTopDefVal (ERel e1 _ e2) = do
     calcTopDefVal (EMul e1 Times e2)
     return VBool
 calcTopDefVal (EAnd e1 e2) = calcTopDefVal2' e1 e2 VBool
-    ("Error: Expression " ++ show e1 ++ " not a boolean.")
-    ("Error: Expression " ++ show e2 ++ " not a boolean.")
+    (eTypeBool e1)
+    (eTypeBool e2)
 calcTopDefVal (EOr e1 e2) = calcTopDefVal (EAnd e1 e2)
-calcTopDefVal _  = lift $ Bad $ "Error: Expression assigned to global variable is not a constant value."
+calcTopDefVal _  = liftError eVarGlobalAss
 
 calcTopDefVal' :: Expr -> Val -> String -> Stt Val
 calcTopDefVal' e v s = do
@@ -386,9 +404,9 @@ calcTopDefVal2' e1 e2 v s1 s2 = do
 
 checkMain :: Env -> Err ()
 checkMain env = case Data.Map.lookup "main" $ fEnv env of
-    Nothing -> Bad $ "Error: Main function not found."
-    Just (t,args,_) -> if t /= Int then Bad $ "Error: Main function return type should be int."
-        else if args /= [] then Bad $ "Error: Main function does not take any arguments."
+    Nothing -> Bad $ eMainNotFound
+    Just (t,args,_) -> if t /= Int then Bad $ eMainRet
+        else if args /= [] then Bad $ eMainArgs
         else return ()
 
 -- Check if every function has a return (excl. void functions)
@@ -399,7 +417,7 @@ checkReturn env = mapM_ checkReturn' $ toList $ fEnv env
 checkReturn' :: (Var, FVal) -> Err ()
 checkReturn' (_,(Void,_,_)) = return ()
 checkReturn' (var,(_,_,b)) = case checkReturnBlock b of
-    Ok _  -> Bad $ "Error: Reached end of non-void function " ++ var ++ "."
+    Ok _  -> Bad $ eNonVoidEnd var
     Bad _ -> return ()
 
 checkReturnBlock :: Block -> Err ()
