a757817bde1a2fb98c14e9b0068ede6280b36e5c
Bartłomiej Karwowski <karwowski.bart@gmail.com>
2019-05-16 15:56:47 +0200

    Fix some stuff to match task

--- zip.sh
+++ zip.sh
@@ -1,13 +1,15 @@
-ZIP_DIR="bk385713"
+ZIP_DIR="bartlomiej_karwowski"
 
 if [ -d "$ZIP_DIR" ]; then
-    rm -r -f bk385713
+    rm -r -f $ZIP_DIR
 fi
 
 mkdir $ZIP_DIR
-mkdir $ZIP_DIR/src
+mkdir $ZIP_DIR/lang
 cp -r prog $ZIP_DIR
-cp src/*.hs $ZIP_DIR/src
-cp src/Makefile $ZIP_DIR/src 
-cp other/decl.pdf $ZIP_DIR
-cp grammar/gram.cf $ZIP_DIR
+cp src/*.hs $ZIP_DIR
+cp src/Makefile $ZIP_DIR 
+cp other/decl.pdf $ZIP_DIR/lang
+cp grammar/gram.cf $ZIP_DIR/lang
+
+cp other/README $ZIP_DIR
